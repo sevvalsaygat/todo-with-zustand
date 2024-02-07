d48cfad7f7595ee18d25e71da15e8c975d3d50e9
sevvalsaygat <sevvallsstt@gmail.com>
2024-02-08 01:05:17 +0300

    feat: create store structure

--- containers/Home/List/index.tsx
+++ containers/Home/List/index.tsx
@@ -1,14 +1,31 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 
 import { Home } from "@app/containers";
+import { useTodoStore } from "@app/stores";
 
 type ListPropTypes = {};
 
 const List: React.FC<ListPropTypes> = () => {
+  const todos = useTodoStore((state) => state.todos);
+
+  useEffect(() => {
+    useTodoStore.persist.rehydrate();
+  }, []);
+
   return (
     <div>
       <div>List</div>
-      <Home.ListItem />
+      <div>
+        {todos.map((todo, i) => {
+          return (
+            <div key={i} className="border rounded-xl mb-5">
+              <Home.ListItem todo={todo} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
---
+++ types/todo-store.d.ts
@@ -0,0 +1,10 @@
+type TodoType = {
+  id: string;
+  title: string;
+  description: string;
+};
+
+type ITodoFormType = {
+  title: string;
+  description: string;
+};
